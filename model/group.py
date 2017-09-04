29ec28f4d424514a674ce1fccb69494095e2e300
KlimenkovDM <KlimenkovDM@yandex.ru>
2017-09-04 18:51:35 +0300

    Добавили новый метод modify_group

--- model/group.py
+++ model/group.py
@@ -1,5 +1,5 @@
 class Group:
-    def __init__(self,name, header, footer):
+    def __init__(self,name = None, header = None, footer = None):
         Group.name = name
         Group.header = header
         Group.footer = footer
